153bd0bec2d38913665b2b41781682568fd12e46
kates <katesgasis@gmail.com>
2012-11-06 14:32:52 +0800

    adding multiline docstring support

--- test/parser-test.js
+++ test/parser-test.js
@@ -5,7 +5,7 @@ var buster = require("buster"),
 
 
 buster.testCase("Parser", {
-	"comment block 0": function() {
+	"parse basic comment block": function() {
 		/*
 		 * @name Klass
 		 * @param {String} name The class name
@@ -35,17 +35,17 @@ buster.testCase("Parser", {
 		assert.equals(ast[0]['function']['args'][0], 'name');
 	},
 
-	"comment block 1": function() {
+	"parse block with multiple params": function() {
 		/**
 		 * @name Klass
 		 * @param {String} name The class name
-		 * @param {Object} options Option
+		 * @param {Object} options Options
 		 *
 		 * @return {Object} This is the return
 		 */
 		//function Klass(num){}
 
-		var code = "/**\n * @name Klass\n * @param {String} name The class name\n * @param {Object} options Options\n *\n * @return {Integer} This is the return\n */\nfunction Klass(num){}";
+		var code = "/**\n * @name Klass\n * @param {String} name The class name\n * @param {Object} options Options\n *\n * @return {Object} This is the return\n */\nfunction Klass(num){}";
 		var ast = parser.parse(code);
 
 		assert.equals(ast[0]['comment']['tags'].length, 4);
@@ -61,5 +61,53 @@ buster.testCase("Parser", {
 		assert.equals(ast[0]['comment']['tags'][2]['value'], 'options');
 		assert.equals(ast[0]['comment']['tags'][2]['type'], 'Object');
 		assert.equals(ast[0]['comment']['tags'][2]['description'], 'Options');
+
+		assert.equals(ast[0]['comment']['tags'][3]['tag'], 'return');
+		assert.equals(ast[0]['comment']['tags'][3]['type'], 'Object');
+		assert.equals(ast[0]['comment']['tags'][3]['description'], 'This is the return');
+
+	},
+
+	"parse block with multiline text": function() {
+		/**
+		 * This is a usage documentation
+		 * in multiline.
+		 *
+		 * This is another paragraph
+		 * that is multiline.
+		 *
+		 * @name Klass
+		 *
+		 * @param {String} name The class name
+		 * @param {Object} options Options
+		 *
+		 * @return {Object} instance
+		 */
+		//function Klass(name, options){}
+
+		var code = "/**\n * This is a usage documentation\n * in multiline.\n *\n * This is another paragraph\n * that is multiline.\n *\n * @name Klass\n *\n * @param {String} name The class name\n * @param {Object} options Options\n *\n * @return {Object} This is the return\n */\nfunction Klass(num){}";
+		
+		var multilineText = "This is a usage documentation\nin multiline.\n\nThis is another paragraph\nthat is multiline.";
+		var ast = parser.parse(code);
+
+		assert.equals(ast[0]['comment']['tags'].length, 4);
+		assert.equals(ast[0]['comment']['tags'][0]['tag'], 'name');
+		assert.equals(ast[0]['comment']['tags'][0]['value'], 'Klass');
+
+		assert.equals(ast[0]['comment']['tags'][1]['tag'], 'param');
+		assert.equals(ast[0]['comment']['tags'][1]['value'], 'name');
+		assert.equals(ast[0]['comment']['tags'][1]['type'], 'String');
+		assert.equals(ast[0]['comment']['tags'][1]['description'], 'The class name');
+
+		assert.equals(ast[0]['comment']['tags'][2]['tag'], 'param');
+		assert.equals(ast[0]['comment']['tags'][2]['value'], 'options');
+		assert.equals(ast[0]['comment']['tags'][2]['type'], 'Object');
+		assert.equals(ast[0]['comment']['tags'][2]['description'], 'Options');
+
+		assert.equals(ast[0]['comment']['tags'][3]['tag'], 'return');
+		assert.equals(ast[0]['comment']['tags'][3]['type'], 'Object');
+		assert.equals(ast[0]['comment']['tags'][3]['description'], 'This is the return');
+
+		assert.equals(ast[0]['comment']['text'], multilineText);
 	}
 });
